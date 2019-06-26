8aa18bb6c283cc900ba4c52b7ba6a6af834bf75b
unknown <kasperkasperfish@gmail.com>
2019-06-26 12:19:26 +0200

    prepare for hosting

--- components/menu/menu-extension.vue
+++ components/menu/menu-extension.vue
@@ -1,18 +1,13 @@
 <template>
   <div>
-    <q-item to="/example">
+    <!-- <q-item to="/example">
       <q-item-side icon="check" color="text2" />
       <q-item-main :label="$t('example.exmaple_menu')" class="text-text1" />
-    </q-item>
-    <q-item to="/your_app">
-      <q-item-side icon="check" color="text2" />
-      <q-item-main label="Go to App" class="text-text1" />
-    </q-item>
+    </q-item> -->
   </div>
 </template>
 
 <script>
-import { mapGetters } from "vuex";
 import { openURL } from "quasar";
 
 export default {
@@ -22,11 +17,6 @@ export default {
   },
   methods: {
     openURL
-  },
-  computed: {
-    ...mapGetters({
-      getSettingByName: "user/getSettingByName"
-    })
   }
 };
 </script>
--- router/routes.js
+++ router/routes.js
@@ -1,25 +1,14 @@
 const routes_extension = [
-  {
-    path: "/",
-    component: () => import("layouts/dacLayout.vue"),
-    children: [
-      {
-        path: "example",
-        component: () => import("../pages/example")
-      }
-    ]
-  },
-  {
-    path: "/your_app",
-    component: () => import("../layouts/appLayout.vue"),
-    children: [
-      { path: "", component: () => import("../pages/app_page_1.vue") },
-      {
-        path: "app_page1",
-        component: () => import("../pages/app_page_1.vue")
-      }
-    ]
-  }
+  // {
+  //   path: "/",
+  //   component: () => import("layouts/dacLayout.vue"),
+  //   children: [
+  //     {
+  //       path: "example",
+  //       component: () => import("../pages/example")
+  //     }
+  //   ]
+  // }
 ];
 
 export default routes_extension;
